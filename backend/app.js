26e0cf93e8c067eef19a23015004325b682193a8
AyeshaWaheed01 <146517105+AyeshaWaheed01@users.noreply.github.com>
2023-12-25 16:30:34 +0000

    Added Connection String

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,18 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
+const connection = () => {
+    const uri = 'mongodb+srv://fa20bse008:fa20bse008@cluster0.1sfpbsf.mongodb.net/student?retryWrites=true&w=majority';
 
+    mongoose.connect(uri);
+
+    const db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', () => {
+        console.log('Connected to the database');
+    });
+}
 
 
 
