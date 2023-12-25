2394e0e063457b8a6cfb163f2dde21244eeb2773
AyeshaWaheed01 <146517105+AyeshaWaheed01@users.noreply.github.com>
2023-12-25 16:52:23 +0000

    Added put to update student

--- backend/Routes/studentRoute.js
+++ backend/Routes/studentRoute.js
@@ -39,38 +39,37 @@ router.get('/', async (req, res) => {
   });
 
 
-  router.put('/students/:id', async (req, res) => {
-    try {
-      const studentId = req.params.id;
-      const { name, batch_no, course_name, roll_no, semester, grade } = req.body;
-  
-      // Validate required fields
-      if (!name || !batch_no || !course_name || !roll_no || !semester || !grade) {
-        return res.status(400).json({ error: 'All fields are required' });
-      }
-  
-      const updatedStudent = await Student.findByIdAndUpdate(
-        studentId,
-        {
-          name,
-          batch_no,
-          course_name,
-          roll_no,
-          semester,
-          grade,
-        },
-        { new: true } // Return the updated document
-      );
-  
-      if (!updatedStudent) {
-        return res.status(404).json({ error: 'Student not found' });
-      }
-  
-      res.json(updatedStudent);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+ app.put('/students/:id', async (req, res) => {
+  try {
+    const studentId = req.params.id;
+    const { name, batch_no, course_name, roll_no, semester, grade } = req.body;
+
+    // Validate required fields
+    if (!name || !batch_no || !course_name || !roll_no || !semester || !grade) {
+      return res.status(400).json({ error: 'All fields are required' });
     }
-  });
-  
+
+    const updatedStudent = await Student.findByIdAndUpdate(
+      studentId,
+      {
+        name,
+        batch_no,
+        course_name,
+        roll_no,
+        semester,
+        grade,
+      },
+      { new: true } // Return the updated document
+    );
+
+    if (!updatedStudent) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    res.json(updatedStudent);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = router
