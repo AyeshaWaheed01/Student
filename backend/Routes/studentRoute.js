04476b5193f2263f784e83c535cfd7945aff78ba
AyeshaWaheed01 <146517105+AyeshaWaheed01@users.noreply.github.com>
2023-12-25 16:51:50 +0000

    Added Post route to add student

--- backend/Routes/studentRoute.js
+++ backend/Routes/studentRoute.js
@@ -12,4 +12,65 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.post('/add', async (req, res) => {
+    try {
+      const { name, batch_no, course_name, roll_no, semester, grade } = req.body;
+  
+      // Validate required fields
+      if (!name || !batch_no || !course_name || !roll_no || !semester || !grade) {
+        return res.status(400).json({ error: 'All fields are required' });
+      }
+  
+      const newStudent = new Student({
+        name,
+        batch_no,
+        course_name,
+        roll_no,
+        semester,
+        grade,
+      });
+  
+      await newStudent.save();
+  
+      res.status(201).json(newStudent);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+
+  router.put('/students/:id', async (req, res) => {
+    try {
+      const studentId = req.params.id;
+      const { name, batch_no, course_name, roll_no, semester, grade } = req.body;
+  
+      // Validate required fields
+      if (!name || !batch_no || !course_name || !roll_no || !semester || !grade) {
+        return res.status(400).json({ error: 'All fields are required' });
+      }
+  
+      const updatedStudent = await Student.findByIdAndUpdate(
+        studentId,
+        {
+          name,
+          batch_no,
+          course_name,
+          roll_no,
+          semester,
+          grade,
+        },
+        { new: true } // Return the updated document
+      );
+  
+      if (!updatedStudent) {
+        return res.status(404).json({ error: 'Student not found' });
+      }
+  
+      res.json(updatedStudent);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+
 module.exports = router
